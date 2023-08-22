ac119e24651982ed0f37ed3b7816e2eca1517349
andyphancode <andyphanpxh@gmail.com>
2023-08-22 13:57:31 -0700

    finished functions

--- queue.js
+++ queue.js
@@ -17,9 +17,39 @@ class Queue {
     this.size = 0;
   }
 
+  get(idx) {
+    let current = this.first;
+    let count = 0;
+
+    while (current !== null && count !== idx) {
+      count++;
+      current = current.next
+    }
+
+    return current;
+  }
+
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    let newNode = new Node(val);
+    
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+      this.size++;
+    } else if (this.size === 1) {
+      let currentNode = this.first;
+      currentNode.next = newNode;
+      this.last = newNode;
+      this.size++;
+    } else {
+      let currentNode = this.get(this.size-1);
+      currentNode.next = newNode;
+      this.last = newNode;
+      this.size++;
+    }
+
 
   }
 
@@ -27,19 +57,41 @@ class Queue {
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    if (this.size === 0) {
+      throw new Error("Queue is empty");
+    } else if (this.size === 1) {
+      let currentNode = this.first;
+      this.first = null;
+      this.last = null;
+      this.size--;
+      return currentNode.val;
+    } else {
+      let currentNode = this.first;
+      this.first = currentNode.next;
+      this.size--;
+      return currentNode.val;
+    }
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    if(this.size === 0){
+      throw new Error("Queue is empty");
+    } else {
+      let currentNode = this.first;
+      return currentNode.val;
+    }
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
--- stack.js
+++ stack.js
@@ -20,26 +20,59 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    let newNode = new Node(val);
+    
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+      this.size++;
+    } else {
+      let currentNode = this.first;
+      newNode.next = currentNode;
+      this.first = newNode;
+      this.size++;
+    } 
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (this.size === 0) {
+      throw new Error("Stack is empty");
+    } else if (this.size === 1) {
+      let currentNode = this.first;
+      this.first=null;
+      this.last=null;
+      this.size--;
+      return currentNode.val;
+    } else {
+      let currentNode = this.first;
+      this.first = this.first.next;
+      this.size--;
+      return currentNode.val;
+    }
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    if(this.size === 0){
+      throw new Error("Stack is empty");
+    } else {
+      let currentNode = this.first;
+      return currentNode.val;
+    }
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
